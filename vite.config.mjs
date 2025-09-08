8b1e37477ea03c7b06c35c47c71608e274dfc617
Steve Fallet <steve.fallet@divtec.ch>
2025-09-08 16:50:01 +0200

    ./ par défaut

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/2024-SFA-TTI-JS-Vue-Vuetify-Pokedex/',
+  base: process.env.VITE_BASE_URL || './',
   plugins: [
     VueRouter(),
     Layouts(),
